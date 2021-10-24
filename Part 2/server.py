dcff99a65f93c052f5ce6feb4971eb1f5c004bf2
David Dinkevich <david.dinkevich@gmail.com>
2021-10-24 22:49:30 +0300

    Implemented server code

---
+++ Part 2/server.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 24 21:12:17 2021
+
+@author: Roei Gehasi and David Dinkevich
+"""
+
+import sys
+import socket
+
+# Get program args
+port_num = int(sys.argv[1])
+
+# Open socket 
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                         # IPV4
+
+# Bind the IP address and the port number
+s.bind(('', port_num))
+
+while True:
+    # Wait and listen for info, max data size 1024
+    data, addr = s.recvfrom(1024)
+    # Print what we received
+    print("Server: ", str(data), addr)
+    # Return exactly what we received
+    s.sendto(data, addr)
+    
