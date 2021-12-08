bfc50dd66e90e4c8c98b1e5c109c3208fb94012e
David Dinkevich <david.dinkevich@gmail.com>
2021-12-08 10:44:48 +0200

    Fixed bug where nonempty directories weren't recursively moved

--- ex2/Part2/lib.py
+++ ex2/Part2/lib.py
@@ -4,29 +4,18 @@ import os
 import time
 
 MSG_LEN_NUM_BYTES = 4
-decode_next_msg = True
-
-def is_dir(relative_path):
-    abs_path = os.path.abspath(relative_path)
-    return os.path.isdir(abs_path)
-
-
-def get_abs_path(path):
-    return os.path.abspath(path)
-
 
 def send_token(socket, args, encode=True):
     #assert len(args) > 0, "send_token: length of args in send_token must be > 0"
     if encode:
         for arg in args:
-            encoded = arg.encode()
-            len_bytes = len(encoded).to_bytes(MSG_LEN_NUM_BYTES, 'little')
+            len_bytes = len(arg).to_bytes(MSG_LEN_NUM_BYTES, 'little')
             #print("Sending message: ", msg)
-            socket.sendall(len_bytes + encoded)
+            socket.sendall(len_bytes + arg.encode())
     # in case its pure data and not files or dirs
     else:
         assert len(args) == 1, "send_token: encode=False but len(args) != 1"
-
+        len_bytes = len(args[0]).to_bytes(MSG_LEN_NUM_BYTES, 'little')
         socket.sendall(args[0])
         #socket.sendall(SEP_CHAR.encode())
 
@@ -44,17 +33,14 @@ def get_token(socket, buff, decode=True, num_bytes_to_read=-1):
             #print('Got raw: ', msg_len, data.decode())
             try:
                 decoded = data.decode()
-                if decode_next_msg:
-                    buff.append(decoded)
-                else:
-                    buff.append(data)
+                buff.append(decoded)
             except:
                 buff.append(data)
                 #print('Uh oh... tried to decode:', decoded[:15], '...', decoded[-15:])
-
+        
         print('Got message: ', num_bytes_to_read, buff[-1][:15], '...', buff[-1][-15:])
        # buff.extend(final_chunks)
-
+        
 
     # whether its empty or not, we want to return one command_token from the buff list we have 😀'
     if len(buff) > 0:
@@ -63,31 +49,37 @@ def get_token(socket, buff, decode=True, num_bytes_to_read=-1):
     return buff, None
 
 def send_file(my_socket, cmd, full_file_path, relative_path):
-    file_size = str(os.path.getsize(full_file_path))
-    send_token(my_socket, [cmd, relative_path, file_size])
+    global decode_next_msg
 
+    file_size = os.path.getsize(full_file_path)
+    send_token(my_socket, [cmd, relative_path, str(file_size)])
+    if file_size == 0:
+        return
+    
     with open(full_file_path, 'rb') as f:
         data = f.read(2048)
         while len(data) > 0:
+            #send_token(my_socket, [str(len(data))])
             send_token(my_socket, [data], encode=False)
+#            send_token(my_socket, [SEP_CHAR.encode()], encode=False)
+
             #my_socket.sendall(data)
             data = f.read(2048)
-
+    
 def rcv_file(my_socket, my_buff, abs_path):
     my_buff, size = get_token(my_socket, my_buff)
-
+    
     size = int(size)
     while size > 0:
-        chunk_size = min(size, 2048)
-        size -= chunk_size
-        my_buff, data = get_token(my_socket, my_buff, decode=False, num_bytes_to_read=chunk_size)
-        #data = my_socket.recv(2048)
+        chunk_size = min(size, 1024)
+        #size -= chunk_size
+        my_buff, data = get_token(my_socket, my_buff, num_bytes_to_read=chunk_size)
+        #data = my_socket.recv(chunk_size)
         # Read content and write to file
         size -= len(data)
         print('Need ', size, ' expected, ', chunk_size, ' got: ', len(data))
         print('Trying to write: ', data[:20], '...', data[-20:])
         write_data(abs_path, data)
-    
         
 def write_data(abs_path, data):
     with open(abs_path, 'ab') as f:
@@ -140,7 +132,7 @@ def move_folder(move_dir_path, new_path):
         return
 
     # If folder is empty, simply rename it
-    if len(os.listdir(move_dir_path)) == 0:
+    if is_folder_empty(move_dir_path) == 0:
         os.renames(move_dir_path, new_path)
         return
 
@@ -162,12 +154,12 @@ def move_folder(move_dir_path, new_path):
             move_folder(os.path.join(root, folder), os.path.join(new_path, folder))
      
 
+def is_folder_empty(dir_path):
+    for root, d_names, f_names in os.walk(dir_path, topdown=True):
+        for file in f_names:
+            return False
+        for folder in d_names:    
+            return False
+    return True
 
-def send_all_dirs_and_files(socket, dirs, files, dest_folder):
-    for rel_path in (dirs + files):
-        abs_path = os.path.join(dest_folder, rel_path)
-        if rel_path in dirs:
-            send_token(socket, ['mkdir', rel_path])
-        else:
-            send_file(socket, 'mkfile', abs_path, rel_path)
 
